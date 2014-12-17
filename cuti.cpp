2c1bdfc907a88885db01c2ff4173b4a0c452d053
Kirill Igumenshchev <kirill@berkeley.edu>
2014-12-17 15:07:27 -0800

    cuti ranges for row headers work, both headers displayed on output

--- cuti.cpp
+++ cuti.cpp
@@ -24,13 +24,23 @@ auto parse_header_in(T hin){
       names_to_match.emplace_back(t);
     }
   }
-  return make_tuple(names_to_match,mcols_rbegins,mcols_rends);
+  return make_tuple(names_to_match,beginning_of_range_to_match,ending_of_range_to_match);
 }
 
+template<typename E, typename B, typename Counter,typename Op>
+void compare_with_range(E element,B& bounds,Counter& in_range,Op op){
+  auto comp_rb = find_if(begin(bounds),end(bounds),[=](regex b){return regex_match(element,b);});
+  while(comp_rb!=end(bounds)){
+    bounds.erase(comp_rb);
+    in_range = op(in_range,1); //++in_range or --
+    comp_rb = find_if(begin(bounds),end(bounds),[=](regex b){return regex_match(element,b);});
+  }
+}
 
 int main(int argc, char* argv[]){
   vector<regex> mrows,mrows_rbegins,mrows_rends;////note set,unordered_multiset don't work with regex due to absense of comparision or hashing
   tie(mrows,mrows_rbegins,mrows_rends) = parse_header_in(argv[1]);
+  unsigned in_row_range=0;
   vector<string> rows;
   vector<size_t> rowsi;
 
@@ -43,25 +53,15 @@ int main(int argc, char* argv[]){
   getline(cin,colss);
   istringstream colsiss(colss);
   size_t icol=1;
-  unsigned in_range=0;
+  unsigned in_col_range=0;
   for(string c;getline(colsiss,c,',');) {
-    auto comp_rb = find_if(begin(mcols_rbegins),end(mcols_rbegins),[=](regex r){return regex_match(c,r);});
-    while(comp_rb!=end(mcols_rbegins)){
-      mcols_rbegins.erase(comp_rb);
-      ++in_range;
-      comp_rb = find_if(begin(mcols_rbegins),end(mcols_rbegins),[=](regex r){return regex_match(c,r);});
-    }
-    if(any_of(begin(mcols),end(mcols),[=](regex r){return regex_match(c,r);})||in_range){
+    compare_with_range(c,mcols_rbegins,in_col_range,plus<unsigned>());
+    if(any_of(begin(mcols),end(mcols),[=](regex r){return regex_match(c,r);})||in_col_range){
       cols.push_back(c);
       colsi.push_back(icol);
     }
     ++icol;
-    auto comp_re = find_if(begin(mcols_rends),end(mcols_rends),[=](regex r){return regex_match(c,r);});
-    while(comp_re!=end(mcols_rends)){
-      mcols_rends.erase(comp_re);
-      --in_range;
-      comp_re = find_if(begin(mcols_rends),end(mcols_rends),[=](regex r){return regex_match(c,r);});
-    }
+    compare_with_range(c,mcols_rends,in_col_range,minus<unsigned>());
   }
 
   vector<vector<string>> data;
@@ -72,7 +72,9 @@ int main(int argc, char* argv[]){
     //// parse  row  header (row's numbers)
     string first_cell;
     getline(issl,first_cell,',');
-    if(any_of(begin(mrows),end(mrows),[=](regex r){return regex_match(first_cell,r);})){
+    cells.push_back(first_cell);
+    compare_with_range(first_cell,mrows_rbegins,in_row_range,plus<unsigned>());
+    if(any_of(begin(mrows),end(mrows),[=](regex r){return regex_match(first_cell,r);})||in_row_range){
       icol=1;
       if(find(begin(colsi),end(colsi),icol)!=end(colsi)){
         cells.push_back(first_cell);
@@ -89,18 +91,22 @@ int main(int argc, char* argv[]){
       data.push_back(cells);
       ++irow;
     }
+    compare_with_range(first_cell,mrows_rends,in_row_range,minus<unsigned>());
   }
 
+  cout << "_,";
   for(size_t i=0; i<cols.size(); ++i) {
     cout << cols[i];
     if(i!=cols.size()-1)
       cout << ",";
   }
   cout << endl;
-  for(auto i:data){
-    for(size_t j=0; j<i.size(); ++j) {
-      cout << i[j];
-      if(j!=i.size()-1)
+  //for(auto i:data){
+  for(size_t i=0; i<data.size();++i){
+    cout << data[i][0]<<",";
+    for(size_t j=1; j<data[i].size(); ++j) {
+      cout << data[i][j];
+      if(j!=data[i].size()-1)
         cout << ",";
     }
     cout << endl;
