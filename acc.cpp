9e8163d3e2a1da3c7cf799130ca880417af533a2
Kirill Igumenshchev <kirill@berkeley.edu>
2014-12-22 15:57:52 -0800

    added checks for missing values, nan, inf. also implemented stdev

--- acc.cpp
+++ acc.cpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <cmath>
+#include <cfloat>
+#include <stdexcept>
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/stats.hpp>
 #include <boost/accumulators/statistics/mean.hpp>
 #include <boost/accumulators/statistics/moment.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
 
 using namespace std;
 using namespace boost::accumulators;
@@ -23,13 +27,42 @@ int main()
   string tmp_tok; getline(iss_header,tmp_tok,',');
   for(string tok; getline(iss_header,tok,',');col_names.push_back(tok));
 
-  vector<accumulator_set<double, stats<tag::mean, tag::moment<2> > >> accs(col_names.size());
+  vector<accumulator_set<double, features<
+    tag::count,
+    tag::mean,
+    tag::variance
+    >>> accs(col_names.size());
+  vector<size_t> missing(col_names.size(),0);
+  vector<size_t> infs(col_names.size(),0);
+  vector<size_t> nans(col_names.size(),0);
   for(string line;getline(cin,line);){
     istringstream issl(line);
     string idx; getline(issl,idx,',');
     size_t icol =0;
     for(string tok;getline(issl,tok,','); ){
-      ref(accs[icol++])(stod(tok));
+      double x;
+      if(tok.empty())
+        missing[icol]++;
+      else{
+        try {
+          x = stod(tok);
+        } catch (const std::invalid_argument&) {
+          cerr << "Error: stod convertion of the cell is invalid.\n";
+          throw;
+        } catch (const std::out_of_range&) {
+          cerr << "Error: stod convertion of the cell is out of range.\n";
+          throw;
+        }
+        if(std::isfinite(x))
+          ref(accs[icol])(x);
+        else if(std::isinf(x))
+          infs[icol]++;
+        else if(std::isnan(x))
+          nans[icol]++;
+        else
+          cerr << "error: caticorization of input cell didn't work.\n";
+      }
+      ++icol;
     }
   }
 
@@ -39,16 +72,22 @@ int main()
     else cout << " ,# acc_name";
   }
   cout << endl;
-  //for(size_t i=0; i<accs.size();++i){
-    //cout << mean(accs[i]);
-    //if(i!=col_names.size()-1) cout << ',';
-    //else cout << " ,# count";
-  //}
+  for(size_t i=0; i<accs.size();++i){
+    cout << count(accs[i]);
+    if(i!=col_names.size()-1) cout << ',';
+    else cout << " ,# count";
+  }
   cout << endl;
   for(size_t i=0; i<accs.size();++i){
     cout << mean(accs[i]);
     if(i!=col_names.size()-1) cout << ',';
     else cout << " ,# mean";
+  }
+  cout << endl;
+  for(size_t i=0; i<accs.size();++i){
+    cout << sqrt(variance(accs[i]));
+    if(i!=col_names.size()-1) cout << ',';
+    else cout << " ,# stdev";
   }
   cout << endl;
 
