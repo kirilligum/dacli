5f4c4013f5ca663a5c88575cf5968143af4b9231
Kirill Igumenshchev <kirill@berkeley.edu>
2014-12-23 00:13:07 -0800

    added 25th,50th, 75 percentile by lower

--- acc.cpp
+++ acc.cpp
@@ -12,6 +12,9 @@
 #include <boost/accumulators/statistics/mean.hpp>
 #include <boost/accumulators/statistics/moment.hpp>
 #include <boost/accumulators/statistics/variance.hpp>
+#include <boost/accumulators/statistics/min.hpp>
+#include <boost/accumulators/statistics/extended_p_square.hpp>
+#include <boost/accumulators/statistics/max.hpp>
 
 using namespace std;
 using namespace boost::accumulators;
@@ -19,7 +22,6 @@ using namespace boost::accumulators;
 int main()
 //int main(int argc, char *argv[])
 {
-  //// by default, display (based on pandas describe) count, mean, std, min, 25, 50, 75th precentile, and max for each column excluding NaN. output number of nans and missing values.
 
   string header; getline(cin,header);
   vector<string> col_names;
@@ -27,11 +29,17 @@ int main()
   string tmp_tok; getline(iss_header,tmp_tok,',');
   for(string tok; getline(iss_header,tok,',');col_names.push_back(tok));
 
-  vector<accumulator_set<double, features<
-    tag::count,
-    tag::mean,
-    tag::variance
-    >>> accs(col_names.size());
+  //// by default, display (based on pandas describe) count, mean, std, min, 25, 50, 75th precentile, and max for each column excluding NaN. output number of nans and missing values.
+  vector<double> probs {0.25,0.5,0.75};
+  typedef accumulator_set<double, features<
+    tag::count
+    , tag::mean
+    , tag::variance
+    , tag::min
+    , tag::extended_p_square
+    , tag::max
+    >> acc_type;
+  vector<acc_type> accs(col_names.size(), acc_type( tag::extended_p_square::probabilities = probs));
   vector<size_t> missing(col_names.size(),0);
   vector<size_t> infs(col_names.size(),0);
   vector<size_t> nans(col_names.size(),0);
@@ -66,30 +74,42 @@ int main()
     }
   }
 
-  for(size_t i=0; i<col_names.size();++i){
+  cout << "acc_name,"; for(size_t i=0; i<col_names.size();++i){
     cout << col_names[i];
     if(i!=col_names.size()-1) cout << ',';
-    else cout << " ,# acc_name";
-  }
-  cout << endl;
-  for(size_t i=0; i<accs.size();++i){
-    cout << count(accs[i]);
+  } cout << endl;
+  cout << "count,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::count>(accs[i]);
     if(i!=col_names.size()-1) cout << ',';
-    else cout << " ,# count";
-  }
-  cout << endl;
-  for(size_t i=0; i<accs.size();++i){
-    cout << mean(accs[i]);
+  } cout << endl;
+  cout << "mean,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::mean>(accs[i]);
     if(i!=col_names.size()-1) cout << ',';
-    else cout << " ,# mean";
-  }
-  cout << endl;
-  for(size_t i=0; i<accs.size();++i){
+  } cout << endl;
+  cout << "stdev,"; for(size_t i=0; i<accs.size();++i){
     cout << sqrt(variance(accs[i]));
     if(i!=col_names.size()-1) cout << ',';
-    else cout << " ,# stdev";
-  }
-  cout << endl;
+  } cout << endl;
+  cout << "min,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::min>(accs[i]);
+    if(i!=col_names.size()-1) cout << ',';
+  } cout << endl;
+  cout << "25%,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::extended_p_square>(accs[i])[0];
+    if(i!=col_names.size()-1) cout << ',';
+  } cout << endl;
+  cout << "50%,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::extended_p_square>(accs[i])[1];
+    if(i!=col_names.size()-1) cout << ',';
+  } cout << endl;
+  cout << "75%,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::extended_p_square>(accs[i])[2];
+    if(i!=col_names.size()-1) cout << ',';
+  } cout << endl;
+  cout << "max,"; for(size_t i=0; i<accs.size();++i){
+    cout << extract_result<tag::max>(accs[i]);
+    if(i!=col_names.size()-1) cout << ',';
+  } cout << endl;
 
   return 0;
 }
